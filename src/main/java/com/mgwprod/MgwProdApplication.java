c515d36d6e81b15ca3196f8ddc069213ea3bb4d1
Santiago Weinbinder <santiagoweinbinder@MacBook-Air-de-Santiago.local>
2026-08-27 19:12:18 -0300

    feat: bootstrap Spring Boot project with MySQL connectivity

---
+++ src/main/java/com/mgwprod/MgwProdApplication.java
@@ -0,0 +1,11 @@
+package com.mgwprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgwProdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MgwProdApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mgwprod/MgwProdApplicationTests.java
@@ -0,0 +1,12 @@
+package com.mgwprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgwProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
