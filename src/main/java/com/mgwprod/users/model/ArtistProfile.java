f1e7aef9b5c59daf0f71f86267a7fcdcbb85ccae
Santiago Weinbinder <santiagoweinbinder@MacBook-Air-de-Santiago.local>
2026-08-29 20:23:25 -0300

    refactor(users): hide owning user and add merge-validation on profile entities

--- src/main/java/com/mgwprod/users/model/ArtistProfile.java
+++ src/main/java/com/mgwprod/users/model/ArtistProfile.java
@@ -1,5 +1,6 @@
 package com.mgwprod.users.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,12 +27,15 @@ public class ArtistProfile {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
+    @Size(min = 1, message = "Los géneros no pueden estar vacíos")
     private String genres;
 
+    @Size(min = 1, message = "La biografía no puede estar vacía")
     @Column(columnDefinition = "TEXT")
     private String bio;
 }
